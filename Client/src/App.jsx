f6e235268c156d896ab2178ee78fbd012d79c366
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-26 13:40:36 -0700

    fixing for protype

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -25,15 +25,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Smart Expire</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -46,8 +38,8 @@ function App() {
             ))}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="feel-at-home">
+        Welcome to Hadrian's site!
       </p>
       <div>
         <ButtonComponent />
--- server/main.py
+++ server/main.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from image import send_image
 
 app = Flask(__name__)
-cors = CORS(app, origins='*')
+#cors = CORS(app, origins='*')
+CORS(app)
 
 @app.route("/api/users", methods=['GET'])
 def users():
@@ -22,7 +23,7 @@ def handle_data():
     data = request.get_json() # Access data sent from React
     print(f"Received data from React, in flask: {data}") # log
     #process ie database, normally
-    send_image() #TODO see ai model on desktop, learn more react, make button to submit image and temp store it.
+    send_image()
     return jsonify({'message': 'Data received successfully!'})
 
 @app.route("/login", methods=['POST'])
