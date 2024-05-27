a77d8bd5938b934a987b35363c2046dfd0748b5a
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-27 15:27:50 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 # Replace with actual server details and command
 SERVER_ADDRESS="76.167.195.153"
 USERNAME="hadrian"
-REMOTE_COMMAND="s"  # Example command
+REMOTE_COMMAND="cd /d E:pull_origin.bat" 
+REMOTE_COMMAND1="pull_origin.bat" 
 
 # Establish connection (assuming password authentication)
 echo Enter Password for ssh server: 
 read password
-sshpass -p $password ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER_ADDRESS "$REMOTE_COMMAND"
+sshpass -p $password ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER_ADDRESS "$REMOTE_COMMAND", "$REMOTE_COMMAND1"
 
 # Handle exit status (optional)
 if [ $? -eq 0 ]; then
