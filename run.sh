d917c8d88669ab67327fb69038e374becbd34f31
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-28 08:56:54 -0700

    updates for delopying

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 cd server
 python3 main.py
-#TODO finish code to start backend and lauch neginx also add python virtual envirment
+# python is installed create virtual envirment
+#TODO finish code to start backend and lauch neginx
+# also add python virtual envirment
