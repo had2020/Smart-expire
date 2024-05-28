860710e0947c78f596d860fde9a088295c633405
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-27 19:38:44 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 # Replace with actual server details and command
 SERVER_ADDRESS="76.167.195.153"
 USERNAME="hadrian"
-REMOTE_COMMAND="cd /d E:\Smart_Expire\Smart-expire" 
-REMOTE_COMMAND1="pull_origin.bat" 
+#REMOTE_COMMAND1="cd /d E:\Smart_Expire\Smart-expire" 
+REMOTE_COMMAND2="E:\Smart_Expire\Smart-expire\pull_origin.bat" 
 #TODO add run server and on desktop server for frontend
 
 # Establish connection (assuming password authentication)
 echo Enter Password for ssh server: 
 read password
-sshpass -p $password ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER_ADDRESS "$REMOTE_COMMAND", "$REMOTE_COMMAND1"
-
+#sshpass -p $password ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER_ADDRESS "$REMOTE_COMMAND1"
+sshpass -p $password ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER_ADDRESS "$REMOTE_COMMAND2"
 # Handle exit status (optional)
 if [ $? -eq 0 ]; then
   echo "Remote command executed successfully."
