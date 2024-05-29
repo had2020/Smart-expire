9cfe4e7f1910d6dc36aaa4a5df02df6ffc1d7d7d
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-05-28 18:39:52 -0700

    updates

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ python3 main.py
 # python is installed create virtual envirment
 #TODO finish code to start backend and lauch neginx
 # also add python virtual envirment
+
+# source my_env/bin/activate once in server
